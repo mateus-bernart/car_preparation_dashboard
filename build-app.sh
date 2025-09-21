a00f04e85e7a6c36eed8b84b99cbf3996639f53e
Mateus Davi Bernart <mateusbernart14@gmail.com>
2025-09-21 11:26:41 -0300

    feat: adiciona script de inicialização

---
+++ build-app.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+echo "🔧 Limpando build anterior..."
+rm -rf public/build
+rm -f public/hot
+
+echo "📦 Instalando dependências do PHP..."
+composer install --no-dev --optimize-autoloader
+
+echo "🔁 Rodando migrations..."
+php artisan migrate:fresh --force --seed|| true
+
+echo "🧹 Limpando caches Laravel..."
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+php artisan event:clear
+
+echo "⚙️ Gerando caches Laravel..."
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan event:cache
+
+echo "📦 Instalando dependências do Node..."
+npm ci
+
+echo "🛠 Construindo front-end com Vite..."
+npm run build
+
+echo "✅ Build completo."
