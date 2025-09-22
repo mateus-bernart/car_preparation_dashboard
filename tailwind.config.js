dc69d035f6996ea240272d70696e123174456c22
Mateus Davi Bernart <mateusbernart14@gmail.com>
2025-09-21 22:56:53 -0300

    feat: adds tailwind config for mapping the classes of priority

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+export default {
+    content: ['./resources/**/*.blade.php', './resources/**/*.tsx', './resources/**/*.ts', './resources/**/*.jsx', './resources/**/*.js'],
+    safelist: ['bg-green-500', 'bg-amber-500', 'bg-red-500'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
